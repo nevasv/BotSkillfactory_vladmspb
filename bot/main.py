66db02a551e65b29a38e3bc933b7181e18b46bb4
vladmspb <nevasv@yandex.ru>
2023-01-06 15:59:57 +0300

    C5.1 Введение 050123

--- bot/main.py
+++ bot/main.py
@@ -1,1 +1,25 @@
 # bot
+import telebot
+
+TOKEN = "5635438312:AAHVu-3ToFXfVK1JuNYvz1KvzjUwdI3jfYs"
+
+bot = telebot.TeleBot(TOKEN)
+
+
+# Обрабатываются все сообщения, содержащие команды '/start' or '/help'.
+@bot.message_handler(commands=['start', 'help'])
+def handle_start_help(message: telebot.types.Message):
+    bot.send_message(message.chat.id, f"Привет пришелец, {message.chat.username}")
+
+
+@bot.message_handler(content_types=["voice", ])
+def repeat(message: telebot.types.Message):
+    bot.send_message(message.chat.id, f"Большой брат пишет, говори ещё ))")
+
+
+@bot.message_handler(content_types=['photo', ])
+def say_lmao(message: telebot.types.Message):
+    bot.reply_to(message, 'Nice meme XDD')
+
+
+bot.polling(none_stop=True)
