9044cc9487c3aa3962ab0f844bcc1fed9e54e6c1
vladmspb <nevasv@yandex.ru>
2023-01-06 17:59:31 +0300

    TOKEN

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,12 @@
-# bot
-import telebot
+# bot из задания 5.1 . Введение
 
-TOKEN = "5635438312:AAHVu-3ToFXfVK1JuNYvz1KvzjUwdI3jfYs"
+"""
+Бот принимае соощения /start /help, отвечает на отправленную фотографию и звук
+Создан по материалам задания 5.1 . Введение
+
+"""
+import telebot
+from bot.config import TOKEN
 
 bot = telebot.TeleBot(TOKEN)
 
@@ -12,14 +17,17 @@ def handle_start_help(message: telebot.types.Message):
     bot.send_message(message.chat.id, f"Привет пришелец, {message.chat.username}")
 
 
+# Обрабатываются все сообщения, содержащие команды voice.
 @bot.message_handler(content_types=["voice", ])
 def repeat(message: telebot.types.Message):
     bot.send_message(message.chat.id, f"Большой брат пишет, говори ещё ))")
 
 
+# Обрабатываются все сообщения, содержащие команды photo.
 @bot.message_handler(content_types=['photo', ])
 def say_lmao(message: telebot.types.Message):
     bot.reply_to(message, 'Nice meme XDD')
 
 
+# Запуск полинга
 bot.polling(none_stop=True)
